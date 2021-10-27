ef2d5ef01e8a397f1e182075518b839ffbea413e
kusumeshkant <kushumeshkant2013@gmail.com>
2021-10-27 10:49:49 +0530

    day3 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/home_page.dart';
+import 'package:flutter_tutorial/Screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // initialRoute: "/home";
+      routes: {
+        "/": (context) => LoginPage(),
+        "/Login": (context) => LoginPage(),
+      },
+    );
   }
 }
